fd8d85f3843ea0da086e2315819a9cfdb02b503d
Nathan Porter <nathanporter@outlook.com>
2024-09-17 02:05:44 -0400

    Add ReportingStructure endpoint.

--- src/main/java/com/mindex/challenge/controller/EmployeeController.java
+++ src/main/java/com/mindex/challenge/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.mindex.challenge.controller;
 
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,14 +24,29 @@ public class EmployeeController {
 
     @GetMapping("/employee/{id}")
     public Employee read(@PathVariable String id) {
-        LOG.debug("Received employee create request for id [{}]", id);
+        LOG.debug("Received employee read request for id [{}]", id);
 
         return employeeService.read(id);
     }
 
+    /**
+     * Gets the reporting structure using the employee's given ID
+     *
+     * @param id -  id of the employee
+     * @return the reporting structure of the employee
+     */
+    @GetMapping("/employee/reporting-structure/{id}")
+    public ReportingStructure getReportingStructure(@PathVariable String id) {
+        LOG.debug("Received employee reporting structure request for id [{}]", id);
+
+        Employee employee = employeeService.read(id);
+        int numberOfReports = employeeService.getNumberOfReports(id);
+        return new ReportingStructure(employee, numberOfReports);
+    }
+
     @PutMapping("/employee/{id}")
     public Employee update(@PathVariable String id, @RequestBody Employee employee) {
-        LOG.debug("Received employee create request for id [{}] and employee [{}]", id, employee);
+        LOG.debug("Received employee update request for id [{}] and employee [{}]", id, employee);
 
         employee.setEmployeeId(id);
         return employeeService.update(employee);
